c36306558713d13d114c7122792d30770ce8400e
sudaredd <sudaredd@gmail.com>
2020-12-31 20:55:23 -0500

    some changes

--- src/main/java/ds/algos/CheckAllAlphabets.java
+++ src/main/java/ds/algos/CheckAllAlphabets.java
@@ -4,6 +4,7 @@ public class CheckAllAlphabets {
 
     public static void main(String[] args) {
         System.out.println(checkAllAlphabets("qwertyuioplkjhgfdsAzxcvbnm"));
+        System.out.println(checkAllAlphabets_1("qwertyuioplkjhgfdsAzxcvbnm"));
         System.out.println(checkAllAlphabets("123"));
         System.out.println(checkAllAlphabets("ejuxggfsts"));
         System.out.println(checkAllAlphabets("wyyga"));
@@ -29,6 +30,25 @@ public class CheckAllAlphabets {
         }
 
         return true;
+    }
+
+    public static boolean checkAllAlphabets_1(String str) {
+
+        boolean[] ary = new boolean[26];
+
+        int counter = 0;
+        for (char c : str.toCharArray()) {
+
+            if(c >='A' && c<='Z' && !ary[c - 'A']) {
+                ary[c - 'A'] = true;
+                counter++;
+            } else if (c >= 'a' && c <= 'z'&& !ary[c - 'a']) {
+                ary[c - 'a'] = true;
+                counter++;
+            }
+        }
+
+        return counter==26;
     }
 }
 
---
+++ src/main/java/ds/algos/MinPathTraingle.java
@@ -0,0 +1,35 @@
+package ds.algos;
+
+import java.util.Arrays;
+
+public class MinPathTraingle {
+
+    public static void main(String[] args) {
+        int array[][] = {
+                   {2}, 
+                  {5, 3},
+                 {6, 4, 7},
+                {9, 5, 3, 1}
+        };
+        System.out.println(minPath(array));
+    }
+    
+    public static int minPath(int[][] array) {
+        int dp[] = new int[array.length];
+
+        int length =array[ (array.length-1)].length;
+        
+        for(int i=0; i<length; i++) {
+            dp[i] = array[length-1][i];
+        }
+        System.out.println(Arrays.toString(dp));
+
+        for(int i=length-2; i >0; i--) {
+            for(int j=0; j<array[i].length; j++) {
+                dp[j] = array[i][j]+ Math.min(dp[j], dp[j+1]);
+            }
+            System.out.println(Arrays.toString(dp));
+        }
+        return dp[0];
+    }
+}
