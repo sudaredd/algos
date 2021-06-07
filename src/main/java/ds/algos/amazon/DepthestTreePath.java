de511b67243d4b1915b2215212dbaac2b3304ca4
sudarsana kasireddy <sudaredd@gmail.com>
2021-06-07 16:37:28 -0400

    add Amazon solved problems

--- src/main/java/ds/algos/amazon/DepthestTreePath.java
+++ src/main/java/ds/algos/amazon/DepthestTreePath.java
@@ -1,7 +1,44 @@
 package ds.algos.amazon;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
 public class DepthestTreePath {
     public static void main(String[] args) {
 
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(8);
+        root.right = new TreeNode(12);
+        root.left.right = new TreeNode(9);
+        root.left.left = new TreeNode(7);
+        root.right.left = new TreeNode(11);
+        root.right.right = new TreeNode(13);
+
+        int depth = depthestLevel(root);
+        System.out.println(depth);
+    }
+
+    private static int depthestLevel(TreeNode root) {
+        var queue = new LinkedList<TreeNode>();
+        var level = new LinkedList<Integer>();
+        var map = new LinkedHashMap<Integer, Integer>();
+        int max = Integer.MIN_VALUE;
+        queue.offer(root);
+        level.add(1);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            Integer lev = level.poll();
+            max = Math.max(lev, max);
+            map.compute(lev, (k, v) -> v == null ? 1 : 1 + v);
+            if (node.left != null) {
+                queue.offer(node.left);
+                level.offer(lev + 1);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+                level.offer(lev + 1);
+            }
+        }
+        return map.values().stream().mapToInt(k -> k).max().orElse(0);
     }
 }
---
+++ src/main/java/ds/algos/amazon/TreeNode.java
@@ -0,0 +1,11 @@
+package ds.algos.amazon;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
